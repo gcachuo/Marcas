7c2249d3190ddf582522606b794bb4c3edcc4ade
Memo Cachú <gcachu.o@gmail.com>
2016-07-16 12:03:09 -0500

    ultimos cambios

---
+++ archivo.php
@@ -0,0 +1,20 @@
+        <?php
+        $archivo=$_POST["archivo"];
+        $idMarca=$_POST["idMarca"];
+        ?>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <script src="js/jquery-2.1.4.js"></script>
+        <script src="js/bootstrap.js"></script>
+<script>
+$(function(){
+});
+   function regresar(id){
+                $("#idMarca").val(id);
+                $("#frmSistema").submit();
+            }
+</script>
+<form method="post" action="marcas.php" id="frmSistema">
+            <input type="hidden" name="idMarca" value="<?php echo $idMarca?>">
+        </form>
+<a class="btn btn-default" style="cursor:pointer;" onclick="regresar('<?php echo $idMarca?>')">Regresar</a>
+<embed src="<?php echo $archivo;?>?autoplay=1" frameborder="0" style="width: 100%; height: 100vh;">
